da4652040d5d8a072c0431d16408fa47a6e32628
Marc Casian Nicolae <marccasiannicolae@gmail.com>
2017-01-10 22:51:43 +0200

    + add iText-5.0.5 library

--- src/controller/ControllerSectie.java
+++ src/controller/ControllerSectie.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import domain.Candidat;
 import domain.Sectie;
 import domain.ValidatorException;
 
